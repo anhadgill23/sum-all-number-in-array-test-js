4980a9743aceb859979fb74873ee1a5cd0c70262
Anhad Gill <gillanhad23@gmail.com>
2018-04-12 22:43:18 +0000

    Implemented solution using recursion

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+  var sum = 0;
+  array.forEach(element => {
+    if (Array.isArray(element)) {
+      sum += sumItems(element);
+    }
+    else {
+      sum += element;
+    }
+  });
+  return sum;
 }
+console.log(sumItems([2,[3, 4],4,5,6]));
 
 module.exports = sumItems;
